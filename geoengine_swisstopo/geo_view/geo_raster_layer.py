e691044c1ffbad137351cef8f3fa7d38d735dbd8
Stéphane Brunner <stephane.brunner@camptocamp.com>
2023-11-20 15:19:16 +0100

    Support all projections from SwissTopo
    Use the service Capabilities

--- geoengine_swisstopo/geo_view/geo_raster_layer.py
+++ geoengine_swisstopo/geo_view/geo_raster_layer.py
@@ -1,7 +1,15 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+import logging
+
+import requests
+from odoo import api, fields, models
+
+_LOGGER = logging.getLogger(__name__)
+_CAPABILITIES_URL = (
+    "https://wmts.geo.admin.ch/EPSG/{matrix_set}/1.0.0/WMTSCapabilities.xml"
+)
 
 
 class GeoRasterLayer(models.Model):
